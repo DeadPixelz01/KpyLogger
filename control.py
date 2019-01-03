c03dd9b55e7a107ebc3ac6052ae68fb383c35dc7
DeadPixelz01 <deadpixelz01@gmail.com>
2019-01-03 09:24:15 -0600

    Added further internal doc

--- control.py
+++ control.py
@@ -11,8 +11,11 @@ def mouse_control(position):
     mouse.position = position
 
 
+# types out a string using the user's keyboard
 def keyboard_control(typed_phrase):
+    # create a variable called keyboard and set it to use the controller function
     keyboard = Controller()
+    # type out the string
     keyboard.type(typed_phrase)
 
 
