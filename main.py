0a074da3b75f3195782a565bc9954c94427e2370
DeadPixelz01 <deadpixelz01@gmail.com>
2019-01-03 08:48:40 -0600

    Rewrote functions to use 'with' statements and removed hardcoded file names/variables

--- main.py
+++ main.py
@@ -5,20 +5,23 @@
 
 
 # creates and writes to a local text file called 'usr_log'
-def append_to_log():
-    file_a = open('usr_log.txt', 'a')
-    # writes string to file
-    file_a.write('Testing!\n')
-    # close the file
-    file_a.close()
+def append_to_log(file, keystrokes):
+    with open(file, 'a') as file_a:
+        # writes string to file
+        file_a.write(keystrokes + '\n')
 
 
-def read_from_log():
+def read_from_log(file):
     # reads from a local text file called 'usr_log'
-    file_r = open('usr_log.txt', 'r')
-    # creates a variable for the contents of the text file
-    file_data = file_r.read()
-    # prints the contents of the text file
-    print(file_data)
-    # close the file
-    file_r.close()
+    with open(file, 'r') as file_r:
+        # creates a variable for the contents of the text file
+        file_data = file_r.read()
+        # prints the contents of the text file
+        print(file_data)
+
+
+# Testing this on user input
+usr_file = input('Enter a filename: ')
+usr_input = input('Please enter a string: ')
+append_to_log(usr_file, usr_input)
+read_from_log(usr_file)
